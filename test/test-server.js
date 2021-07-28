95f9a9c51a79fdd92aab9760066e07a4e190d061
avueworks <vue.15@osu.edu>
2021-07-28 17:11:17 +0000

    actually fixed test

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to The SQL Spies application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to The SQL Spies Application!</h1>"));
         return done();
       });
 
